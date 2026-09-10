a5b87f4fe394c976270d39cfb151aafaecb99100
Dionigi <105047564+Di0nigi@users.noreply.github.com>
2026-09-10 14:15:18 +0200

    deep restructuring, functions still not properly moved

--- __init__.py
+++ __init__.py
@@ -1,33 +1,21 @@
+# API implementation
 
-## API implementation
-from ScanDL2 import ScanDL2Module
-from ScanDL2 import ScanDLModule
-from ScanDL2 import FixDurModule
+from ScanDL2.scandl2_pkg.scandl2 import ScanDL2
+from ScanDL2.scandl2_pkg.scandl2 import ScanDLModule
+from ScanDL2.scandl2_pkg.scandl2 import FixdurModule
 
-from ScanDL2.ScanDL2Module import ScanDL2
-from ScanDL2.ScanDLModule import ScanDL
-from ScanDL2.FixDurModule import FixDurModel
-
-
-
-# from ScanDL2.scandl2_pkg import FixdurModule
-
-
+#from ScanDL2.scandl2_pkg.utils import 
 
 
 __all__ = [
-    'ScanDL2Module',
-    'ScanDLModule',
-    'FixDurModule',
 
     'ScanDL2',
-    'ScanDL',
-    'FixDurModel',
-    
 
+    'ScanDLModule',
+
+    'FixdurModule',
     
+    'utils'
+
+
 ]
-
-
-
-
