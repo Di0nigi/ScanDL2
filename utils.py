be02fbd65c91684f8caeba681e60cc7b032ba42c
Dionigi <105047564+Di0nigi@users.noreply.github.com>
2026-09-15 13:32:39 +0200

    contants

--- utils.py
+++ utils.py
@@ -10,6 +10,9 @@ from ScanDL2.fix_dur_module.utils_data import get_embeddings_seq2seq,prepare_seq
 
 from ScanDL2.fix_dur_module.scasim import scasim
 
+from ScanDL2.model import ScanDL2
+from ScanDL2.model import ScanDLModule
+
 __all__=[
 
     'padding_and_mask_seq2seq',
@@ -31,5 +34,27 @@ __all__=[
 
 
 
-def infer_Scandl2(type_of_input,path_to_weights):
+def get_scanpath(input,model_params,input_type="text",input_format="sentence",model="ScanDL2"):
+
+    res = {}
+
+    if model == "ScanDL2":
+        model = ScanDL2() 
+    elif model == "ScanDL":
+        model = ScanDLModule()
+
+    if input_type == "text":
+
+        out = model(
+            text_type='sentence',
+            bsz=2,
+            save='scandl2_pkg/test_sentences',
+            filename='sample_output',
+        )
+
+    else:
+        pass
+    
+
+
     return
